d008bf33deaa3f239f9d04d11e723711a2808605
Carter Adams <carteradams@protonmail.com>
2023-01-25 19:33:53 -0500

    monorepo setup for mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { hello } from '../shared/util/strings';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{hello}</Text>
       <StatusBar style="auto" />
     </View>
   );
