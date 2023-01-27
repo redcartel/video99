5184a980c365eb31cc3754633f29abea522db54f
Carter Adams <carteradams@protonmail.com>
2023-01-27 15:46:01 -0500

    graphql serving test mongo data

---
+++ web/src/pages/api/graphql.ts
@@ -0,0 +1,67 @@
+import { gql, ApolloServer } from "apollo-server-micro";
+import { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const uri = process.env.PRIVATE_MONGOuri!;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "https://studio.apollographql.com");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if (req.method === "OPTIONS") {
+        res.end();
+        return false;
+    }
+    console.log(uri);
+    const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+    try {
+        await client.connect();
+    }
+    catch (e) {
+        console.error('error connecting to MongoDB');
+        console.error(e);
+        return res.status(500).send({ error: 'DB connection issue' });
+    }
+
+    const books = await client.db('staging').collection('testCollection').find().toArray();
+
+    const resolvers = {
+        Query: {
+            books: () => books,
+        },
+    };
+
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+
+    await apolloServer.start();
+
+    await apolloServer.createHandler({
+        path: "/api/graphql",
+    })(req, res);
+}
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
